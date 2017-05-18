554c64bb78c78596cdddd6a741d11c4a90a7867e
wellyngtonteixeira <wellyngtonteixeira@gmail.com>
2017-05-18 14:54:55 -0300

    0.0.6
    Criando estrutura para gerenciar Classe Disciplina

---
+++ poscad/admin/models/fields/disciplina.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+JFormHelper::loadFieldClass('list');
+ 
+/**
+ * HelloWorld Form Field class for the HelloWorld component
+ *
+ * @since  0.0.1
+ */
+class JFormFieldDisciplina extends JFormFieldList
+{
+	/**
+	 * The field type.
+	 *
+	 * @var         string
+	 */
+	protected $type = 'Disciplina';
+ 
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return  array  An array of JHtml options.
+	 */
+	protected function getOptions()
+	{
+		$db    = JFactory::getDBO();
+		$query = $db->getQuery(true);
+		$query->select('id,nome');
+		$query->from('#__disciplina');
+		$db->setQuery((string) $query);
+		$messages = $db->loadObjectList();
+		$options  = array();
+ 
+		if ($messages)
+		{
+			foreach ($messages as $message)
+			{
+				$options[] = JHtml::_('select.option', $message->id, $message->nome);
+			}
+		}
+ 
+		$options = array_merge(parent::getOptions(), $options);
+ 
+		return $options;
+	}
+}
+?>

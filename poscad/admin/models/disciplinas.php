dac8959fcc9e2dae2cb7489f8825253115452008
wellyngtonteixeira <wellyngtonteixeira@gmail.com>
2017-05-24 16:19:22 -0300

    0.0.10
    Toolbar funcionando + tradução

---
+++ poscad/admin/models/disciplinas.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_disciplina
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * DisciplinaList Model
+ *
+ * @since  0.0.1
+ */
+class PosCadModelDisciplinas extends JModelList
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $config  An optional associative array of configuration settings.
+	 *
+	 * @see     JController
+	 * @since   1.6
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id',
+				'nome'
+			);
+		}
+ 
+		parent::__construct($config);
+	}
+	/**
+	 * Method to build an SQL query to load the list data.
+	 *
+	 * @return      string  An SQL query
+	 */
+	protected function getListQuery()
+	{
+		// Initialize variables.
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+ 
+		// Create the base select statement.
+		$query->select('*')
+                ->from($db->quoteName('#__disciplina'));
+ 
+ 		// Filter: like / search
+		$search = $this->getState('filter.search');
+ 
+		if (!empty($search))
+		{
+			$like = $db->quote('%' . $search . '%');
+			$query->where('nome LIKE ' . $like);
+		}
+ 
+		// Add the list ordering clause.
+		$orderCol	= $this->state->get('list.ordering', 'nome');
+		$orderDirn 	= $this->state->get('list.direction', 'asc');
+ 
+		$query->order($db->escape($orderCol) . ' ' . $db->escape($orderDirn));
+ 
+		return $query;
+	}
+}
+?>
